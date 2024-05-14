3d62d56d9d2a5e4e2679a1402809b8fe9388b83f
1999esther <99013648+1999esther@users.noreply.github.com>
2024-05-14 19:10:37 +0300

    Updates on MyAppState,Question,Answer,Quiz,Result files

--- flutter_app/lib/main.dart
+++ flutter_app/lib/main.dart
@@ -17,6 +17,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
           colorScheme: ColorScheme.fromSwatch(primarySwatch: Colors.green)),
       home: const MyHomePage(title: 'Sample Learning App'),
+      
     );
   }
 }
