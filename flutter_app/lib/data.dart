635d59e35fa2ed3389d93d04aeca1bd98c55d468
1999esther <99013648+1999esther@users.noreply.github.com>
2024-05-29 16:41:34 +0300

    updated data.dart

--- flutter_app/lib/data.dart
+++ flutter_app/lib/data.dart
@@ -1,22 +1,31 @@
 //small json data assigned to variable data to be used in FlashcardScreen widget
 var data = {
   "Alphabet": [
+    {"image": "assets/images/A.jpg", "letter": "A"},
+    {"image": "assets/images/B.jpg", "letter": "B"},
     {"image": "assets/images/C.jpg", "letter": "C"},
-    {"image": "assets/images/D.jpg", "letter": "D"},
     {"image": "assets/images/E.jpg", "letter": "E"},
     {"image": "assets/images/F.jpg", "letter": "F"},
     {"image": "assets/images/G.jpg", "letter": "G"},
     {"image": "assets/images/H.jpg", "letter": "H"},
     {"image": "assets/images/I.jpg", "letter": "I"},
+    {"image": "assets/images/imageJ.jpg", "letter": "J"},
+    {"image": "assets/images/K.jpg", "letter": "K"},
     {"image": "assets/images/L.jpg", "letter": "L"},
-    {"image": "assets/images/imageM.jpg", "letter": "M"},
-    {"image": "assets/images/O.jpg", "letter": "O"},
+    {"image": "assets/images/M.jpg", "letter": "M"},
+    {"image": "assets/images/N.jpg", "letter": "N"},
+  {"image": "assets/images/O.jpg", "letter": "O"},
+    {"image": "assets/images/P.jpg", "letter": "P"},
+    {"image": "assets/images/Q.jpg", "letter": "Q"},
     {"image": "assets/images/R.jpg", "letter": "R"},
-    {"image": "assets/images/S.jpg", "letter": "S"},
+    {"image": "assets/images/S.jpg", "letter": "S  "},
     {"image": "assets/images/T.jpg", "letter": "T"},
+    {"image": "assets/images/U.jpg", "letter": "U"},
     {"image": "assets/images/V.jpg", "letter": "V"},
     {"image": "assets/images/W.jpg", "letter": "W"},
-    {"image": "assets/images/Y.jpg", "letter": "Y"}
+    {"image": "assets/images/X.jpg", "letter": "X"},
+    {"image": "assets/images/Y.jpg", "letter": "Y"},
+    {"image": "assets/images/Z.jpg", "letter": "Z"}
   ],
   "Basics": [
     {"image": "IMAGE PENDING", "letter": "Hello"},
