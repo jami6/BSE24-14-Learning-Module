401bac95cb7dccb37eee4ffa51cd190701369e05
jamilah <nantjami@gmail.com>
2024-06-02 08:55:27 +0300

    Numbers and minor issues have been resolved

--- flutter_app/lib/data.dart
+++ flutter_app/lib/data.dart
@@ -9,16 +9,16 @@ var data = {
     {"image": "assets/images/G.jpg", "letter": "G"},
     {"image": "assets/images/H.jpg", "letter": "H"},
     {"image": "assets/images/I.jpg", "letter": "I"},
-    {"image": "assets/images/imageJ.jpg", "letter": "J"},
+    {"image": "assets/images/J.jpg", "letter": "J"},
     {"image": "assets/images/K.jpg", "letter": "K"},
     {"image": "assets/images/L.jpg", "letter": "L"},
     {"image": "assets/images/M.jpg", "letter": "M"},
     {"image": "assets/images/N.jpg", "letter": "N"},
-  {"image": "assets/images/O.jpg", "letter": "O"},
+    {"image": "assets/images/O.jpg", "letter": "O"},
     {"image": "assets/images/P.jpg", "letter": "P"},
     {"image": "assets/images/Q.jpg", "letter": "Q"},
     {"image": "assets/images/R.jpg", "letter": "R"},
-    {"image": "assets/images/S.jpg", "letter": "S  "},
+    {"image": "assets/images/S.jpg", "letter": "S"},
     {"image": "assets/images/T.jpg", "letter": "T"},
     {"image": "assets/images/U.jpg", "letter": "U"},
     {"image": "assets/images/V.jpg", "letter": "V"},
@@ -28,7 +28,6 @@ var data = {
     {"image": "assets/images/Z.jpg", "letter": "Z"}
   ],
   "Numbers": [
-<<<<<<< HEAD
     {"image": "assets/images/0.jpeg", "letter": "0"},
     {"image": "assets/images/1.jpeg", "letter": "1"},
     {"image": "assets/images/2.jpeg", "letter": "2"},
@@ -40,17 +39,5 @@ var data = {
     {"image": "assets/images/8.jpeg", "letter": "8"},
     {"image": "assets/images/9.png", "letter": "9"},
     {"image": "assets/images/10.jpeg", "letter": "10"}
-=======
-    {"image": "assets/images/1.jpg", "letter": "1"},
-    {"image": "assets/images/2.jpg", "letter": "2"},
-    {"image": "assets/images/3.jpg", "letter": "3"},
-    {"image": "assets/images/4.jpg", "letter": "4"},
-    {"image": "assets/images/4.jpg", "letter": "5"},
-    {"image": "assets/images/6.jpg", "letter": "6"},
-    {"image": "assets/images/4.jpg", "letter": "7"},
-    {"image": "assets/images/4.jpg", "letter": "8"},
-    {"image": "assets/images/4.jpg", "letter": "9"},
-    {"image": "assets/images/4.jpg", "letter": "10"}
->>>>>>> f4dd821ef62c3c0c7374d456e5361f69bbd24c0e
   ],
 };
